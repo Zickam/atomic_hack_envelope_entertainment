e5a133ed4f5ebbb31628798804984bf62c9417f2
John Carrington <kanie.w@mail.ru>
2024-06-16 02:36:33 +0500

    fix chain

--- llama-server/mistral-chain.py
+++ llama-server/mistral-chain.py
@@ -1,13 +1,9 @@
-import os
-
 from langchain_community.llms.llamafile import Llamafile
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-
-
-os.system('llama-server/llamafile-0.8.6/bin/llamafile.exe -m ./TinyLlama-1.1B-Chat-v1.0.Q4_0.llamafile')
+from langchain_core.runnables import RunnablePassthrough, RunnablePick
 
 model_id = 'intfloat/multilingual-e5-large'
 model_kwargs = {'device': 'cpu'}
@@ -22,15 +18,16 @@ llm = Llamafile()
 # Prompt
 prompt = PromptTemplate.from_template(
 """
-Ты эксперт по базе знаний компании РОСАТОМ. Пользователь задает тебе вопрос, ты отвечаешь на него в чате, используя только контекст базы знаний компании, предоставленной тебе в виде файлов. Твоя задача помочь 
+Ты эксперт по базе знаний компании РОСАТОМ.Ты общаешься на русском языке. Пользователь задает тебе вопрос, ты отвечаешь на него в чате, используя только контекст базы знаний компании, предоставленной тебе в виде файлов. Твоя задача помочь 
 пользователю найти ответ на вопрос, и решить его. Все ответы пиши в контексте процессов, регламентов и документов компании РОСАТОМ. Пользователь это сотрудник компании, твоя задача максимально точно и понятно 
 отвечать на вопросы пользователя. Помни, пользователь задает вопросы только в контексте базы знаний компании РОСАТОМ. Отвечай только на заданный вопрос. Не делай повторов предложений. Отвечай так, чтобы 
 пользователь мог самостоятельно решить свой вопрос, используя, приложение, документацию, или сайт компании. В ответе описывай шаги по решению вопроса, если оно есть в базе знаний. Не предлагай 
 пользователю обращаться в техническую поддержку, ты и есть часть технической поддержки. То чего нет в базе знаний компании ты не знаешь, и ответить не можешь. Никогда не пиши вопрос решен, опиши шаги 
-необходимые для решения вопроса.\n\n
-Context:\n {context}?\n
-Question: \n{question}\n
+необходимые для решения вопроса. Подробно раскрой ответ на вопрос.\n\n
 
+
+Context:\n {context} \n
+Question: \n {question}? \n
 Answer:"""
 )
 
@@ -39,11 +36,12 @@ def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 
-chain = {"docs": format_docs} | prompt | llm | StrOutputParser()
 
-vectorstore = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+vectorstore = FAISS.load_local("data/faiss_index", embeddings, allow_dangerous_deserialization=True)
 # Perform similarity search in the vector database based on the user question
-question = "What are the approaches to Task Decomposition?"
-docs = vectorstore.similarity_search(question)
-chain.invoke(docs)
+while True:
+    question = input('> ')
+    chain = RunnablePassthrough.assign(context=RunnablePick("context") | format_docs) | prompt | llm | StrOutputParser()
+    docs = vectorstore.similarity_search(question)
+    print(chain.invoke({"context": docs, "question": question}))
 
