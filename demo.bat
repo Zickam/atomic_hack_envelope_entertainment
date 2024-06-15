2b635cab6977d81738265baa1973b24e89087f95
Almaz <Zickam@mail.ru>
2024-06-16 04:50:49 +0500

    demo fully working but takes a lot of time

--- demo.bat
+++ demo.bat
@@ -1,6 +1,10 @@
-call conda create -n env
-call conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
-call conda install python==3.11 transformers pypdf2 faiss -c pytorch -c conda-forge
+@REM call conda create -n env
+@REM call conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+@REM call conda install python==3.11 transformers pypdf2 faiss -c pytorch -c conda-forge
+call pip install transformers
+call pip install pypdf2
+call pip install faiss-cpu
+call pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 call pip install langchain-community
 call pip install sentence-transformers
 cd src
