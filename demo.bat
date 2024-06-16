223a19aa112d5f27371abacbf0a7cbb80c5ed5da
Almaz <Zickam@mail.ru>
2024-06-16 05:09:49 +0500

    install_dependencies is separate file now

--- demo.bat
+++ demo.bat
@@ -1,12 +1,7 @@
 @REM call conda create -n env
 @REM call conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 @REM call conda install python==3.11 transformers pypdf2 faiss -c pytorch -c conda-forge
-call pip install transformers
-call pip install pypdf2
-call pip install faiss-cpu
-call pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-call pip install langchain-community
-call pip install sentence-transformers
+call install_deps
 cd src
 call python demo.py
 call echo "exiting the app..."
