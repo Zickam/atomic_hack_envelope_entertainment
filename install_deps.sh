52b7922bfc09985588c1780087e815e4430faf11
Almaz <Zickam@mail.ru>
2024-06-16 06:11:12 +0500

    improved readme

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,4 @@
-pip install transformers
-pip install pypdf2
-pip install faiss-cpu
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+conda install python==3.11 transformers pypdf2 faiss -c pytorch -c conda-forge
+conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
 pip install langchain-community
 pip install sentence-transformers
