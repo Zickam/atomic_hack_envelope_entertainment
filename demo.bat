74df6b92fea47b87e3d7752538ae1cf9e305ab43
Almaz <Zickam@mail.ru>
2024-06-16 05:36:01 +0500

    linux demo support added?

--- demo.bat
+++ demo.bat
@@ -1,8 +1,4 @@
-@REM call conda create -n env
-@REM call conda install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
-@REM call conda install python==3.11 transformers pypdf2 faiss -c pytorch -c conda-forge
 call install_deps
 cd src
 call python demo.py
-call echo "exiting the app..."
-@REM call timeout 100000
+call echo "Exiting the app..."
---
+++ install_deps.sh
@@ -0,0 +1,6 @@
+pip install transformers
+pip install pypdf2
+pip install faiss-cpu
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install langchain-community
+pip install sentence-transformers
